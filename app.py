d2a4d4430293c7826250cd72ad25641d3ddb4ab8
Fernando <fernandotrejo125@gmail.com>
2024-06-09 14:25:01 -0300

    delete after 15 seconds testing

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, render_template
 from io import BytesIO
-import os
 import random
 import string
+import threading
+import time
 
 app = Flask(__name__)
 uploads = {}
 
+# Tiempo en segundos para mantener el archivo en memoria antes de eliminarlo
+EXPIRATION_TIME = 15  # 5 minutos
+
+def remove_file_after_timeout(filename):
+    time.sleep(EXPIRATION_TIME)
+    if filename in uploads:
+        del uploads[filename]
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return 'No file part', 400
+        return render_template('error.html', message='No file part'), 400
     file = request.files['file']
     if file.filename == '':
-        return 'No selected file', 400
+        return render_template('no_file_selected.html'), 400
     if file:
         filename = ''.join(random.choices(string.ascii_lowercase + string.digits, k=16))
         file_content = file.read()
         uploads[filename] = file_content
+        # Crear un hilo para eliminar el archivo después de cierto tiempo
+        threading.Thread(target=remove_file_after_timeout, args=(filename,)).start()
         return f'https://cypherapi.vercel.app/uploads/{filename}', 200
 
 @app.route('/uploads/<filename>')
@@ -26,7 +37,11 @@ def uploaded_file(filename):
     if file_content:
         return send_file(BytesIO(file_content), mimetype='image/png')
     else:
-        return 'File not found', 404
+        return render_template('file_not_found.html'), 404
+
+@app.route('/')
+def index():
+    return render_template('index.html'), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
