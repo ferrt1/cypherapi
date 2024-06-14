e664dcc60d03adaca71c42a07fd513760b097f3f
Fernando <fernandotrejo125@gmail.com>
2024-06-14 02:46:23 -0300

    adding containers info

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -15,3 +15,21 @@ document.addEventListener('DOMContentLoaded', function() {
     overlay.classList.remove('open');
   });
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetID = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetID);
+
+    if (targetElement) {
+      const offsetTop = targetElement.offsetTop;
+
+      window.scrollTo({
+        top: offsetTop - 0, // Ajusta el -0 si necesitas un desplazamiento más fino
+        behavior: 'smooth'
+      });
+    }
+  });
+});
